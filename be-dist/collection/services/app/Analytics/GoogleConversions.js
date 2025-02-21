cb11c01a89c1d6ff269987a500048930366594a4
david <chowaifaty.d@gmail.com>
2025-02-21 15:13:34 +0200

    a

--- be-dist/collection/services/app/Analytics/GoogleConversions.js
+++ be-dist/collection/services/app/Analytics/GoogleConversions.js
@@ -1,4 +1,4 @@
-import { injectHTML } from "../../../utils/utils";
+import { injectHTML, injectHTMLAndRunScript } from "../../../utils/utils";
 export class GoogleConversion {
     constructor(conversionId) {
         this.conversionId = conversionId;
@@ -20,27 +20,49 @@ export class GoogleConversion {
         GoogleConversion.initialized = true;
     }
     trackSearch({ from_date, to_date }) {
-        this.trackEvent('search', {
-            from_date,
-            to_date,
-        });
+        const script = `
+    fbq('track', 'Search', {
+      search_string: '${from_date} / ${to_date}'
+    });
+`;
+        injectHTMLAndRunScript(script, 'search');
+        // this.trackEvent('search', {
+        //   from_date,
+        //   to_date,
+        // });
     }
     trackCheckout({ currency, value }) {
-        this.trackEvent('begin_checkout', {
-            value,
-            currency,
-        });
+        const script = `
+        fbq('track', 'InitiateCheckout', {
+          value: ${value},
+          currency: ${currency}
+         });
+        `;
+        injectHTMLAndRunScript(script, 'checkout');
+        // this.trackEvent('begin_checkout', {
+        //   value,
+        //   currency,
+        // });
     }
-    static isGtagAvailable() {
-        return typeof window !== 'undefined' && typeof window.gtag === 'function';
-    }
+    // private static isGtagAvailable(): boolean {
+    //   return typeof window !== 'undefined' && typeof window.gtag === 'function';
+    // }
     trackPurchase({ currency, roomnights, transaction_id, value }) {
-        this.trackEvent('purchase', {
-            value,
-            currency,
-            roomnights,
-            transaction_id,
-        });
+        // this.trackEvent('purchase', {
+        //   value,
+        //   currency,
+        //   roomnights,
+        //   transaction_id,
+        // });
+        const script = `
+      fbq('track', 'Purchase', {
+        value: ${value},
+        currency: ${currency},
+        roomnights:${roomnights},
+        transaction_id:${transaction_id}
+      });
+  `;
+        injectHTMLAndRunScript(script, 'purchase');
     }
     /**
      * Sends an event to Google Analytics.
@@ -48,15 +70,14 @@ export class GoogleConversion {
      * @param eventName The name of the event.
      * @param eventParams Optional parameters to send with the event.
      */
-    trackEvent(eventName, eventParams) {
-        if (GoogleConversion.isGtagAvailable()) {
-            window.gtag('event', eventName, eventParams);
-            console.log(`GA4 Event Tracked: ${eventName}`, eventParams || {});
-        }
-        else {
-            console.warn('Google Analytics (gtag) is not available. Event not tracked:', eventName, eventParams || {});
-        }
-    }
+    // private trackEvent(eventName: string, eventParams?: { [key: string]: any }): void {
+    //   if (GoogleConversion.isGtagAvailable()) {
+    //     window.gtag!('event', eventName, eventParams);
+    //     console.log(`GA4 Event Tracked: ${eventName}`, eventParams || {});
+    //   } else {
+    //     console.warn('Google Analytics (gtag) is not available. Event not tracked:', eventName, eventParams || {});
+    //   }
+    // }
     trackConversion() { }
 }
 GoogleConversion.initialized = false;
--- be-dist/types/services/app/Analytics/GoogleConversions.d.ts
+++ be-dist/types/services/app/Analytics/GoogleConversions.d.ts
@@ -6,7 +6,6 @@ export declare class GoogleConversion implements IConversion {
     init(): void;
     trackSearch({ from_date, to_date }: SearchParams): void;
     trackCheckout({ currency, value }: CheckoutParams): void;
-    private static isGtagAvailable;
     trackPurchase({ currency, roomnights, transaction_id, value }: GooglePurchaseParams): void;
     /**
      * Sends an event to Google Analytics.
@@ -14,6 +13,5 @@ export declare class GoogleConversion implements IConversion {
      * @param eventName The name of the event.
      * @param eventParams Optional parameters to send with the event.
      */
-    private trackEvent;
     trackConversion(): void;
 }
